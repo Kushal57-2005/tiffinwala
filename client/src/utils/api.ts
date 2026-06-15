e1e880e1a221362a8fed35bb8c9fb2b68ee1a531
Kushal <Kushalwaykole57@gmail.com>
2026-06-15 14:13:29 +0530

    fix cors

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://tiffinwala-kjfo.onrender.com/',
+    baseURL: 'https://tiffinwala-kjfo.onrender.com/api/',
     withCredentials: true,
 });
